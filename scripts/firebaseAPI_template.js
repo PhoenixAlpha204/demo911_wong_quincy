6509df1beea71873b6c36f1ebb092e93be7e2b6c
PhoenixAlpha204 <68132454+PhoenixAlpha204@users.noreply.github.com>
2023-02-28 09:00:23 -0800

    Initial commit

---
+++ scripts/firebaseAPI_template.js
@@ -0,0 +1,15 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    //keys go here 
+  
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
