194ecd01277a9b24b4e2298677707c78239393de
Quincy Wong <quincywong00@gmail.com>
2023-03-07 09:36:06 -0800

    finished demo 10 tasks

--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -19,3 +19,47 @@ function displayHikeInfo() {
         } );
 }
 displayHikeInfo();
+
+function saveHikeDocumentIDAndRedirect(){
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
+
+function populateReviews() {
+    let hikeCardTemplate = document.getElementById("reviewCardTemplate");
+    let hikeCardGroup = document.getElementById("reviewCardGroup");
+
+    //let params = new URL(window.location.href) //get the url from the searbar
+    //let hikeID = params.searchParams.get("docID");
+    var hikeID = localStorage.getItem("hikeDocID");
+    
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection("reviews").where( "hikeDocID", "==", hikeID).get()
+        .then(allReviews => {
+            reviews=allReviews.docs;
+            console.log(reviews);
+            reviews.forEach(doc => {
+                var title = doc.data().title; //gets the name field
+                var level = doc.data().level; //gets the unique ID field
+                var season = doc.data().season;
+                var description = doc.data().description; //gets the length field
+                var flooded = doc.data().flooded;
+                var scrambled = doc.data().scrambled;
+                var time = doc.data().timestamp.toDate();
+                console.log(time)
+
+                let reviewCard = hikeCardTemplate.content.cloneNode(true);
+                reviewCard.querySelector('.title').innerHTML = title;     //equiv getElementByClassName
+                reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();    //equiv getElementByClassName
+                reviewCard.querySelector('.level').innerHTML = `level: ${level}`;
+                reviewCard.querySelector('.season').innerHTML = `season: ${season}`;
+                reviewCard.querySelector('.scrambled').innerHTML = `scrambled: ${scrambled}`;  //equiv getElementByClassName
+                reviewCard.querySelector('.flooded').innerHTML = `flooded: ${flooded}`;  //equiv getElementByClassName
+                reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
+                hikeCardGroup.appendChild(reviewCard);
+            })
+        })
+}
+populateReviews();
